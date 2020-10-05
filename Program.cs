9860660fa341f77003fe21d6e2449f507304339c
Axel <axel.lefort@hotmail.fr>
2020-10-05 14:43:24 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShifuCommon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
